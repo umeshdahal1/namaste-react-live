b859ba92f6e78557de252e9b5289686d578e882d
Umesh <umeshdahal.np@gmail.com>
2023-02-24 22:10:07 +0545

    second commit

--- App.js
+++ App.js
@@ -1,4 +1,25 @@
-const heading = React.createElment(
+/*
+** These are something that Parcel did for us. 
+** HMR-  Hot Module Reload: It instantly load any change in file. 
+** File Watcher Algorithm- C++ : File Watcher Algorithm keep track of all the files which are changing in real time and it tells the server reload.
+** Bundling 
+** Minify
+** Cleaning our code: Remove console.log
+** Dev and Production Build
+** Super Fast build algorithm
+** Image Optimization
+** Caching while developement
+** Compression
+** Compatible with older version of browser
+** HTTPS 
+
+
+*/
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
   "h1",
   {
     id: "title",
@@ -6,7 +27,7 @@ const heading = React.createElment(
   "Heading 1"
 );
 
-const heading2 = React.createElment(
+const heading2 = React.createElement(
   "h2",
   {
     id: "title",
@@ -14,7 +35,7 @@ const heading2 = React.createElment(
   "Heading 2"
 );
 
-const container = React.createElment(
+const container = React.createElement(
   "div",
   {
     id: "container",
